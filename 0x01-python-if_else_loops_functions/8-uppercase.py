af2d811248a3ebcb463770775a708a03f06f6de8
FaruqBalogun <123133306+FaruqBalogun@users.noreply.github.com>
2023-06-08 01:37:30 +0100

    Delete 0x01-python-if_else_loops_functions directory

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python3
-def uppercase(s):
-    for c in s:
-        print("{:c}".format(ord(c) - 32), end="")
-    print()
--- 0x01-python-if_else_loops_functions/README.md
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/python3
-0x01. Python - if/else, loops, functions
